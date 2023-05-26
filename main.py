86a6289b57a85ab26a73a67f2a96218a4542d319
Atanas <atanas925@gmail.com>
2023-05-26 16:00:16 +0300

    removed pycache

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from restaurant.restaurant import Restaurant
 def main():
     restaurant = Restaurant.get_instance()
 
-    # Place orders
     restaurant.place_order("кралски", "чеснов")
     restaurant.place_order("класически", "кетчуп")
     restaurant.place_order("вегетариански", "чеснов") 
